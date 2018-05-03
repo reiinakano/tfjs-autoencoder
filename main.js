f5cabd9d10c1a0fb0f8673ceee30677558f8a701
Reiichiro Nakano <reiichiro.s.nakano@gmail.com>
2018-05-03 18:48:46 +0900

    fix code

--- main.js
+++ main.js
@@ -22,42 +22,9 @@ class Main {
 
     // Initialize buttons
     this.newDigit = document.getElementById('new-digit');
-    this.newDigit.onclick = () => {
-      // TODO: Don't load this in immediately. Add it with noise.
-      let randInt = Math.floor(Math.random() * 1002);
-      let temp = new Image();
-      temp.src = 'test_digits/' + randInt + '.png';
-      temp.onload = () => {
-        if (this.inputTensor) {
-          this.inputTensor.dispose();
-        }
-        this.inputTensor = tf.tidy(() =>
-          tf.fromPixels(temp, 1).toFloat().div(tf.scalar(255))
-        );
-        tf.toPixels(this.inputTensor, this.originalImg);
-      }
-    };
+    this.newDigit.onclick = () => this.updateInputTensor();
     this.newNoise = document.getElementById('new-noise');
-    this.newNoise.onclick = () => {
-      if (this.distortedTensor) {
-        this.distortedTensor.dispose();
-      }
-      this.distortedTensor = tf.tidy(() => {
-        let noiseTensor = tf.randomNormal([28, 28, 1], 0, 0.5);
-        return noiseTensor.add(this.inputTensor).clipByValue(0, 1);
-      });
-      tf.toPixels(this.distortedTensor, this.noisyImg);
-    };
-    this.fixImg = document.getElementById('fix-img');
-    this.fixImg.onclick = () => {
-      if (this.fixedTensor) {
-        this.fixedTensor.dispose();
-      }
-      this.fixedTensor = tf.tidy(() => {
-        return this.model.predict(this.distortedTensor.expandDims()).squeeze();
-      });
-      tf.toPixels(this.fixedTensor, this.restoredImg);
-    };
+    this.newNoise.onclick = () => this.updateNoiseTensor();
 
     tf.loadModel('model/model.json').then((model) => {
       this.model = model;
@@ -70,7 +37,52 @@ class Main {
    * Sets up UI elements.
    */
   enableGeneration() {
+    this.noiseTensor = tf.randomNormal([28, 28, 1], 0, 0.5);
+    this.updateInputTensor();
+  }
 
+  updateInputTensor() {
+    // TODO: Don't load this in immediately. Add it with noise.
+    let randInt = Math.floor(Math.random() * 1002);
+    let temp = new Image();
+    temp.src = 'test_digits/' + randInt + '.png';
+    temp.onload = () => {
+      if (this.inputTensor) {
+        this.inputTensor.dispose();
+      }
+      this.inputTensor = tf.tidy(() =>
+        tf.fromPixels(temp, 1).toFloat().div(tf.scalar(255))
+      );
+      tf.toPixels(this.inputTensor, this.originalImg);
+      this.updateDistortedTensor();
+    }
+  }
+
+  updateNoiseTensor() {
+    this.noiseTensor.dispose();
+    this.noiseTensor = tf.randomNormal([28, 28, 1], 0, 0.5);
+    this.updateDistortedTensor();
+  }
+
+  updateDistortedTensor() {
+    if (this.distortedTensor) {
+      this.distortedTensor.dispose();
+    }
+    this.distortedTensor = tf.tidy(() => {
+      return this.noiseTensor.add(this.inputTensor).clipByValue(0, 1);
+    });
+    tf.toPixels(this.distortedTensor, this.noisyImg);
+    this.updateFixedTensor();
+  }
+
+  updateFixedTensor() {
+    if (this.fixedTensor) {
+      this.fixedTensor.dispose();
+    }
+    this.fixedTensor = tf.tidy(() => {
+      return this.model.predict(this.distortedTensor.expandDims()).squeeze();
+    });
+    tf.toPixels(this.fixedTensor, this.restoredImg);
   }
 }
 
